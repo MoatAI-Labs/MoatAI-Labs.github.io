afffa589ca8b86fae1d79a6be4b612c91a6d08ce
Kiggins26 <andrewkiggins2000@gmail.com>
2025-08-23 20:03:45 +0100

    good enough for now

--- assets/js/main.js
+++ assets/js/main.js
@@ -106,8 +106,7 @@
 
 			// Vars.
 				var $form = document.querySelectorAll('#signup-form')[0],
-					$submit = document.querySelectorAll('#signup-form input[type="submit"]')[0],
-					$message;
+					$submit = document.querySelectorAll('#signup-form input[type="submit"]')[0];
 
 			// Bail if addEventListener isn't supported.
 				if (!('addEventListener' in $form))
